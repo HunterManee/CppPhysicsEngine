6c8c6ea6c79098486bf8b3db93dfdd6cd88699d1
huntermanee <HunterManee3A33@outlook.com>
2026-05-09 11:08:56 -0400

    Debug Update

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -13,8 +13,6 @@ using namespace std;
 
 #include "engine/Entity.h"
 
-#include "engine/JSonOutput.h"
-
 #include "engine/WebsocketOutput.hpp"
 
 
@@ -64,51 +62,4 @@ int main() {
     }
 
 
-}
-
-void MainHold() {
-//Server Test
-    JSonOutput jsonOutput;
-
-    int totalEntities = Random::getRandomInt(5,10);
-    Entity* entities = new Entity[totalEntities];
-    for(int i = 0; i < totalEntities; i++) {
-        Point2 randPosition = {
-            Random::getRandomDouble(-400, 400),
-            Random::getRandomDouble(-300, 300)
-        };
-        Transform transform{randPosition};
-
-        Collider* collider;
-        switch (Random::getRandomInt(0,1))
-        {
-        case 0:
-            collider = new CircleCollider();
-            break;
-        case 1:
-            collider = new PolygonCollider();
-            break;
-        }
-
-        entities[i] = Entity{i, transform, collider};
-    }
-
-
-    // --- MAIN LOOP ---
-    const std::chrono::milliseconds frameTime(16); //~60 FPS
-    while(true) {
-        auto start = std::chrono::steady_clock::now();
-
-        // --- OUTPUT JSON ---
-        jsonOutput.outputEntities(entities, totalEntities);
-
-        // IMPORTANT: ensure newline + flush inside outputEntities !
-
-        // --- FRAME TIMING ---
-        auto elapsed = std::chrono::steady_clock::now() - start;
-        if (elapsed < frameTime) {
-            std::this_thread::sleep_for(frameTime - elapsed);
-        }
-
-    }
 }
